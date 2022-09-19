bbecc10e8588f93977c526d9ea9a254c703bb6d8
Your Namegit config --global user.name Veronika <veronika.tarasova.98@gmail.com>
2022-09-19 19:32:01 +0300

    added src

---
+++ src/Lessons/lesson17/Main.java
@@ -0,0 +1,8 @@
+package Lessons.lesson17;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("Alex", 33, false);
+        person1.toString();
+    }
+}
